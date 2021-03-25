7a78c4c7ee172980c7f29c6def5c01ef10b9fb66
Nneka Tielman <khalie_nne@yahoo.com>
2021-03-25 21:30:06 +0100

    dev homepage effects done

--- src/components/DevHomePage.js
+++ src/components/DevHomePage.js
@@ -3,27 +3,67 @@ import bone from '../images/dev-home-images/bone.png';
 
 const DevHomePage = () => {
 
+    // set initial cursor position
     const currentMousePos = {
         x: -1,
         y: -1
     };
 
+    const maskPos = {
+        x: -1,
+        y: -1
+    };
+
+    const eyesPos = {
+        x: 0,
+        y: 0
+    };
+
+    let windowScroll = {};
+
     const imageRef = useRef();
+    const eyeRef1 = useRef();
+    const eyeRef2 = useRef();
 
     return (
         <div className='flesh-bg'
             onMouseMove={(event) => {
-                currentMousePos.x = event.pageX - 75;
-                currentMousePos.y = event.pageY - 75;
+                // on page scroll get amount scrolled
+                windowScroll = imageRef.current.getBoundingClientRect();
 
-                imageRef.current.style.MaskPositionX = currentMousePos.x + 'px';
-                imageRef.current.style.webkitMaskPositionX = currentMousePos.x + 'px';
+                currentMousePos.x = event.pageX;
+                currentMousePos.y = event.pageY;
 
-                imageRef.current.style.MaskPositionY = currentMousePos.y + 'px';
-                imageRef.current.style.webkitMaskPositionY = currentMousePos.y + 'px';
+                //update xray mask positioning
+                maskPos.x = currentMousePos.x - 75;
+                maskPos.y = currentMousePos.y - windowScroll.y - 75;
+                imageRef.current.style.MaskPositionX = maskPos.x + 'px';
+                imageRef.current.style.webkitMaskPositionX = maskPos.x + 'px';
 
+                imageRef.current.style.MaskPositionY = maskPos.y + 'px';
+                imageRef.current.style.webkitMaskPositionY = maskPos.y + 'px';
+
+                // pupils positioning
+                eyesPos.x = currentMousePos.x * 100 / window.innerWidth;
+                eyesPos.y = currentMousePos.y * 100 / window.innerHeight;
+
+                eyeRef1.current.style.left = (eyesPos.x / 2) + '%';
+                eyeRef1.current.style.top = (eyesPos.y / 2) + '%';
+                eyeRef1.current.style.transform = `translateY(${eyesPos.y}%) translateX(${eyesPos.x}%)`;
+
+                eyeRef2.current.style.left = (eyesPos.x / 2) + '%';
+                eyeRef2.current.style.top = (eyesPos.y / 2) + '%';
+                eyeRef2.current.style.transform = `translateY(${eyesPos.y}%) translateX(${eyesPos.x}%)`;
             }}>
             <img ref={imageRef} src={bone} alt='background x-ray vision' className='bone-bg' />
+            <div className='eyes'>
+                <div className='eye'>
+                    <div ref={eyeRef1} className='pupil'></div>
+                </div>
+                <div className='eye'>
+                    <div ref={eyeRef2} className='pupil'></div>
+                </div>
+            </div>
         </div>
     );
 }
