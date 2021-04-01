2e02113226ee9dd09aa42bd9224d06bc78375b6e
Nneka Tielman <khalie_nne@yahoo.com>
2021-04-01 23:36:44 +0200

    added about me text

--- src/components/aboutPage/DesignAbout.js
+++ src/components/aboutPage/DesignAbout.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import AboutText from './AboutText';
 import { useSelector } from 'react-redux';
 
 const DesignAbout = () => {
 
     const devMode = useSelector(state => state.devMode);
+    const arrowRef = useRef();
 
     return (
         <div className={devMode ? 'hide' : 'about-container des'}>
             <h1>About Me</h1>
             <div className='about-content'>
-                <article className='about-text'>
-                    <h3>Part Unicorn</h3>
-                    <p>lorem ipsum</p>
+                <article className='about-text' onScroll={() => {
+                    arrowRef.current.style.animation = "fadeOut 1s ease forwards";
+                }}>
+                    <h3>Part Designer</h3>
+                    <i ref={arrowRef} className="fas fa-arrow-down helper-arrow"></i>
+                    <AboutText />
                 </article>
                 <figure className='about-image-container'>
                     <img className='avatar des' src={process.env.PUBLIC_URL + '/assets/images/about/designAvatar.png'} alt='Avatar of Nneka' />
--- src/components/aboutPage/DevAbout.js
+++ src/components/aboutPage/DevAbout.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import AboutText from './AboutText';
 import { useSelector } from 'react-redux';
 
 const DevAbout = () => {
 
     const devMode = useSelector(state => state.devMode);
+    const arrowRef = useRef();
 
     return (
         <div className={devMode ? 'about-container dev' : 'hide'}>
@@ -13,9 +15,12 @@ const DevAbout = () => {
                     <img className='avatar dev' src={process.env.PUBLIC_URL + '/assets/images/about/devAvatar.png'} alt='Avatar of Nneka' />
                     <figcaption className='about-caption'>Depiction of Nika "C-137" cooking up a master plan</figcaption>
                 </figure>
-                <article className='about-text'>
-                    <h3>Part Mastermind</h3>
-                    <p>lorem ipsum</p>
+                <article className='about-text' onScroll={() => {
+                    arrowRef.current.style.animation = "fadeOut 1s ease forwards";
+                }}>
+                    <h3>Part Developer</h3>
+                    <i ref={arrowRef} className="fas fa-arrow-down helper-arrow"></i>
+                    <AboutText />
                 </article>
             </div>
         </div>
